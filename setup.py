0c6b3582f18c37e3569873aba46019f2816ab983
Thais Ribeiro <thaisribeiro@softbox.com.br>
2020-12-02 09:07:02 -0300

    setup

--- setup.py
+++ setup.py
@@ -1,23 +1,15 @@
-#!/usr/bin/env python
+from setuptools import setup
 
-import os
+with open("README.md", "r") as fh:
+    readme = fh.read()
 
-from setuptools import find_packages, setup
-
-here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.md'), encoding='utf-8') as fp:
-    README = fp.read()
-
-with open(os.path.join(here, 'VERSION')) as version_file:
-    VERSION = version_file.read().strip()
-
-excluded_packages = ["docs", "tests", "tests.*"]
-
-setup(
-    name='heimdall_bank_validate',
-    version=VERSION,
-    description="Python implementation for bank account validation.",
-    long_description=README,
+setup(name='heimdall_bank_validate',
+    description='Python implementation for bank account validation.',
+    version='0.0.1',
+    url='https://github.com/thaisribeiro/heimdall',
+    license='MIT License',
+    author='Thais Ribeiro',
+    long_description=readme,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -31,12 +23,9 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8'
     ],
-    keywords='heimdall bank validate',
-    author='Thais Ribeiro',
+    long_description_content_type="text/markdown",
     author_email='thaisribeirodn@gmail.com',
-    url='https://github.com/thaisribeiro/heimdall',
-    license='MIT License',
-    packages=find_packages(exclude=excluded_packages),
-    platforms=["any"],
-    python_requires=">=3.5"
-)
+    keywords='heimdall bank validate',
+    description=u'Exemplo de pacote PyPI',
+    packages=['heimdall_bank_validate'],
+    python_requires=">=3",)
